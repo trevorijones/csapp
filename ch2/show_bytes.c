ff38f6bd5216c863c49554f73b3dd3884e2ea25d
charlesborges <charles.borges@eptica.com>
2017-01-19 06:57:00 +0100

    Implementing is_little_endian

--- ch2/show_bytes.c
+++ ch2/show_bytes.c
@@ -14,8 +14,14 @@ void show_int(int x) {
     show_bytes((byte_pointer) &x, sizeof(int));
 }
 
+// returns 1 if the system is little endian or 0 if big endian
 int is_little_endian() {
-    return 0;
+    // int is 4 bytes either on 32 and 64 bits systems
+    unsigned int x = 1;
+    // 01 00 00 00 in little endian
+    // 00 00 00 01 in big endian
+    // grab 1rst byte
+    return *((byte_pointer) &x);
 }
 
 
@@ -23,9 +29,21 @@ void main() {
     show_int(1);
     show_int(15);
     show_int(16);
-    //when byte_pointer is a char *, this is printed as FFFFFFFF 00 00 00
+    //when byte_pointer is a char * instead of unsigned char *, this is printed as FFFFFFFF 00 00 00
+    //there is a sign extention when FF is considered signed to int.
     show_int(255);
     show_int(256);
+    printf("little endian: %d\n", is_little_endian());
+    
+    //2.59
+    int x = 0x89ABCDEF;
+    int y = 0x76543210;
+    //   => 0x765432EF
+    int z = is_little_endian() ? x : y; 
+    show_int(z);
+
+    
+
 }
 
 
