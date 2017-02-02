62ab651434028911d98ff143fee2d5fba2b5e740
charlesborges <charles.borges@eptica.com>
2017-02-02 07:02:20 +0100

    2.612.61

--- ch2/bit_int.c
+++ ch2/bit_int.c
@@ -11,4 +11,15 @@ int get_msb(int x) {
 int main() {
   printf("%.2X \n", get_msb(0x12345678));
   printf("%.2x \n", get_msb(0x78563412));
+  //2.61
+
+  //Any bit of x equals 1
+  int x=0xff;
+  //!x is 0 if any bit is 1
+  //!!x is 1 if !x is 0
+  printf("%d",!!x);
+
+  //Any bit of x is 0
+  //~x is 0 if all bit are 1. x is all 1 => ~x is all 0
 }
+
